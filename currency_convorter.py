156ecd314fbc825e7ae86cc40500a333b69e6df5
Adarshkumar61 <adarshb2k61@gmail.com>
2025-03-09 20:19:24 +0530

    created a curr convoter

---
+++ currency_convorter.py
@@ -0,0 +1,12 @@
+import requests
+API_URL = "https://api.exchangerate-api.com/v4/latest/USD"
+response = requests.get(API_URL)
+rates = response.json()["rates"]
+
+amount = float(input("Enter amount in USD: "))
+currency = input("Enter currency code (e.g., INR, EUR): ").upper()
+
+if currency in rates:
+    print(f"{amount} USD = {amount * rates[currency]} {currency}")
+else:
+    print("Currency not supported.")
