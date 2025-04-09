546c1252f311bb91c826513ac1598054cbc6eed5
Adarshkumar61 <adarshb2k61@gmail.com>
2025-04-09 19:36:07 +0530

    updated

--- gussing_game.py
+++ gussing_game.py
@@ -3,7 +3,7 @@ import random
 def guess():
     no_guess = random.randint(1, 100)
     guess = None
-    while guess != no_guess:
+    while guess != no_guess: 
         guess = int(input("Enter a no bw 1 and 100: "))
         if guess < no_guess:
             print("Too low...")
